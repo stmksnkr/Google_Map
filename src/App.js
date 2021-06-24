3fda2c86237aa5eebf7fa8ff6a837a1a5e0bdf91
satyam kumar <49091346+stmksnkr@users.noreply.github.com>
2021-06-24 21:08:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//AIzaSyBIsqJ5qTnSjssy8Q50DgfCiB9v-qCicAA
+import React from "react";
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps integration</h1>
+      <GoogleMap />
+    </div>
+  );
+}
+
+export default App;
